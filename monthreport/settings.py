ab43f0533c52f9d6943698d8fe6284140f166a07
lisawang(王林燕) <lisawang@futunn.com>
2019-05-08 19:29:51 +0800

    update database setting

--- monthreport/settings.py
+++ monthreport/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'load',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,14 @@ WSGI_APPLICATION = 'monthreport.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'load',
+        'USER': 'root',
+        'PASSWORD': 'CDBfutu@2016$',
+        'HOST': '172.24.31.154',
+        'PORT': '13357',
     }
 }
 
