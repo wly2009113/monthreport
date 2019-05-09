73f4d1b96c277223eb9a7341be20ebf84213473f
lisawang(王林燕) <lisawang@futunn.com>
2019-05-09 15:15:38 +0800

    修改数据库名称

--- load/models.py
+++ load/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Load(models.Model):
+    id = models.AutoField
+    ip = models.IPAddressField
+    business_id = models.IntegerField
+    business_name = models.CharField(max_length = 50)
+    load =models.IntegerField
+
+
+
