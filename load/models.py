3cdcb3c716eb497ba1931679d93d2dca2384af17
lisawang(王林燕) <lisawang@futunn.com>
2019-05-09 17:01:09 +0800

    增加models建表

--- load/models.py
+++ load/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+import django.utils.timezone as timezone
 
 # Create your models here.
 class Load(models.Model):
-    id = models.AutoField
-    ip = models.IPAddressField
-    business_id = models.IntegerField
-    business_name = models.CharField(max_length = 50)
-    load =models.IntegerField
+    ip = models.GenericIPAddressField(blank= False,verbose_name='IP')
+    business_id = models.IntegerField(blank= False, verbose_name='业务模块id')
+    business_name = models.CharField(blank= True,max_length = 50,default='',verbose_name='业务模块')
+    load = models.IntegerField(blank= False,verbose_name='机器负载')
+    create_time = models.DateTimeField( auto_now_add =True,verbose_name='创建时间')
+
+    #后台列表显示时，显示IP.
+    def __unicode__(self):
+        return self.ip
 
 
 
--- monthreport/settings.py
+++ monthreport/settings.py
@@ -85,6 +85,10 @@ DATABASES = {
         'PASSWORD': 'CDBfutu@2016$',
         'HOST': '172.24.31.154',
         'PORT': '13357',
+        'OPTIONS':{
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset':'utf8mb4',
+        },
     }
 }
 
